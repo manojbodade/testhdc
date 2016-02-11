77b084358bee00a1e437839882e641c729b201b8
RyanCi <ryan.angga.p@gmail.com>
2016-02-11 14:25:14 +0700

    update

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	. "github.com/frezadev/hdc/hive"
 	// "os"
 )
@@ -13,6 +13,6 @@ func main() {
 	h = HiveConfig("192.168.0.223:10000", "default", "developer", "b1gD@T@")
 	q := "select * from sample_07 limit 5;"
 	result, e := h.Exec(q)
-	//fmt.Printf("error: \n%s\n", e)
-	//fmt.Printf("result: \n%s\n", result)
+	fmt.Printf("error: \n%s\n", e)
+	fmt.Printf("result: \n%s\n", result)
 }
